c64df624fe8e1eb2babd37505dc0fc8a1268988e
Kabelo Tsiane <kabelo@uchicago.edu>
2025-06-25 14:14:41 -0500

    more cleaning

---
+++ utils/corrs.py
@@ -0,0 +1,70 @@
+import os
+
+import pandas as pd
+import numpy as np
+import utils.data as datutils
+
+
+def unique_redshifts(mah_df_dict: dict[pd.DataFrame]) -> list:
+    zs = [x['Redshift'].to_list() for x in mah_df_dict.values()]
+    redshifts = []
+    for z in zs:
+        redshifts.extend(z)
+
+    redshifts = sorted(list(set(redshifts)))
+
+    return redshifts
+
+
+def build_ma_corrs(mah_df_dict: dict[pd.DataFrame],
+                   df0: pd.DataFrame) -> list:
+    corrs_list = []
+    filtered_z = []
+    params_dict = {}
+    redshifts = unique_redshifts(mah_df_dict)
+    for z in redshifts:
+        mah_df = pd.DataFrame(columns=['ID', 'M/M0'])
+        for region in mah_df_dict.keys():
+            row = mah_df_dict[region].loc[mah_df_dict[region]
+                                          ['Redshift'] == z, ['M/M0']]
+            row['ID'] = region
+            if not row.empty:
+                mah_df = pd.concat([mah_df, row], ignore_index=True)
+
+        if len(mah_df) < 200:
+            continue
+
+        mah_df.set_index('ID', inplace=True)
+        filtered_z.append(z)
+
+        df = mah_df.merge(df0, on='ID', how='inner')
+        params_dict[z] = df
+        corrs = df.corr(method='spearman')
+        corrs_list.append(corrs)
+
+    return params_dict, corrs_list, filtered_z
+
+
+def build_am_corrs(am: np.ndarray, mbins: np.ndarray, sm_df: pd.DataFrame,
+                   mah_dir: str = 'data/gadgetx3k_20/AHFHaloHistory') -> list:
+    mah_files = sorted(os.listdir(mah_dir))
+    ids = [datutils.find_id(f) for f in mah_files]
+    am_df = pd.DataFrame(am)
+    am_df['ID'] = ids
+    am_df.set_index('ID', inplace=True)
+
+    filtered_mbins = []
+    corrs_list = []
+    ind_cols = am_df.columns
+    for col in ind_cols:
+        mah_df = am_df[col].dropna()
+        if len(mah_df) < 100:
+            continue
+
+        filtered_mbins.append(mbins[col])
+
+        df = pd.concat([mah_df, sm_df], join='inner', axis=1)
+
+        corrs = df.corr(method='spearman')
+        corrs_list.append(corrs)
+    return corrs_list, filtered_mbins
